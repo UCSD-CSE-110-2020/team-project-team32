9db5bf33677ffe04f9442cdf5eea4f064d58ea5e
Alexandra Michael <aemichae@ucsd.edu>
2020-02-08 21:18:47 -0800

    #30 - Started on walk screen

---
+++ app/src/main/java/com/example/cse110_project/user_routes/WalkActivity.java
@@ -0,0 +1,16 @@
+package com.example.cse110_project.user_routes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cse110_project.R;
+
+public class WalkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walk);
+    }
+}
