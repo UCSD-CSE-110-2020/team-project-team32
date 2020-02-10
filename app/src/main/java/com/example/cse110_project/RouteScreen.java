c52a8d719c026e85b87fdfbb5758f95c2b8d7323
rframirez <rframire@ucsd.edu>
2020-02-10 12:18:00 -0800

    added new route button on route. list implementation attempt

--- app/src/main/java/com/example/cse110_project/RouteScreen.java
+++ app/src/main/java/com/example/cse110_project/RouteScreen.java
@@ -9,16 +9,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.cse110_project.user_routes.CustomListAdapter;
 
 public class RouteScreen extends AppCompatActivity{
+
+    String[] nameArray = {"Octopus","Pig","Sheep","Rabbit","Snake","Spider" };
+
+    String[] infoArray = {
+            "8 tentacled monster",
+            "Delicious in rolls",
+            "Great for jumpers",
+            "Nice in a stew",
+            "Great for shoes",
+            "Scary."
+    };
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.route_screen);
 
+        CustomListAdapter whatever = new CustomListAdapter(this, nameArray, infoArray);
+        listView = (ListView) findViewById(R.id.listviewID);
+        listView.setAdapter(whatever);
 
+
+        //listView = (ListView) findViewById(R.id.route_list_view);
         // Implementation of button event to route screen
         final Button launchToHomeScreen = (Button) findViewById(R.id.button_routeToHome);
 
