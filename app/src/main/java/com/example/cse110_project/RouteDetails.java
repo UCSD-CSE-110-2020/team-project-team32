6a459ec9c8412fd3f678ed7fdb028866ef273004
Reesha Rajen <rajen.reese@gmail.com>
2020-02-12 23:44:11 -0800

    Favorite a Route [I] #12 implementation

--- app/src/main/java/com/example/cse110_project/RouteDetails.java
+++ app/src/main/java/com/example/cse110_project/RouteDetails.java
@@ -3,10 +3,13 @@ package com.example.cse110_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RouteDetails extends AppCompatActivity {
 
@@ -22,6 +25,7 @@ public class RouteDetails extends AppCompatActivity {
         myText.setText(savedExtra);
 
         final Button launchToRouteScreen = (Button) findViewById(R.id.button_backToRoutes);
+        final ImageButton markAsFavorite = (ImageButton) findViewById(R.id.button_favorites);
 
         launchToRouteScreen.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,11 +33,27 @@ public class RouteDetails extends AppCompatActivity {
                 launchToRouteActivity();
             }
         });
+
+        // User marks route as favorite
+        markAsFavorite.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                launchToFavorites();
+            }
+        });
     }
 
     public void launchToRouteActivity() {
         Intent intent = new Intent(this, RouteScreen.class);
         startActivity(intent);
+    }
+
+    // This is a Toast message that displays whenever Favorites button is pressed
+    public void launchToFavorites() {
+        Toast.makeText(getApplicationContext(),
+                "Added to Favorites!",
+                Toast.LENGTH_LONG)
+        .show();
     }
 
 }
