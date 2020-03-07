418a6cd4cc3eec81d1a7b1bcb25f5f9b15d3e311
Alexandra Michael <aemichae@ucsd.edu>
2020-03-07 13:46:37 -0800

    #82, #88 - Added WalkScheduler to interface with db

---
+++ app/src/main/java/com/example/cse110_project/team/WalkScheduler.java
@@ -0,0 +1,24 @@
+package com.example.cse110_project.team;
+
+import com.example.cse110_project.WWRApplication;
+import com.example.cse110_project.user_routes.Route;
+
+import java.time.LocalDateTime;
+
+public class WalkScheduler {
+    public void createScheduledWalk(Route route, LocalDateTime dateTime, String creatorId,
+                                    Team team) {
+        ScheduledWalk walk = new ScheduledWalk(route, dateTime, creatorId, team);
+        team.setScheduledWalk(walk);
+        WWRApplication.getDatabase().updateTeam(team);
+    }
+
+    public void updateScheduledWalk(Team team) {
+        WWRApplication.getDatabase().updateTeam(team);
+    }
+
+    public void removeScheduledWalk(Team team) {
+        team.setScheduledWalk(null);
+        WWRApplication.getDatabase().updateTeam(team);
+    }
+}
