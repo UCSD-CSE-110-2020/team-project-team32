10356763429a62d7a9cde6e60a6e979310cc0b37
Reesha Rajen <rajen.reese@gmail.com>
2020-03-10 14:00:47 -0700

    Update #90

--- app/src/main/java/com/example/cse110_project/dialogs/ProposeWalkDialog.java
+++ app/src/main/java/com/example/cse110_project/dialogs/ProposeWalkDialog.java
@@ -4,21 +4,28 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cse110_project.R;
 import com.example.cse110_project.WWRApplication;
+import com.example.cse110_project.user_routes.Route;
 import com.example.cse110_project.user_routes.User;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import static com.example.cse110_project.RouteDetailsActivity.ROUTE_INDEX_KEY;
+
 public class ProposeWalkDialog {
 
+    private EditText routeName;
     private User user;
     private AppCompatActivity activity;
+    private Route route; // route
 
     public ProposeWalkDialog(AppCompatActivity activity) {
         user = WWRApplication.getUser();
@@ -29,6 +36,11 @@ public class ProposeWalkDialog {
         // Get prompts.xml view
         LayoutInflater layoutInflater = LayoutInflater.from(activity);
         View promptView = layoutInflater.inflate(R.layout.dialog_propose_walk, null);
+
+        //fixme
+        TextView routeName = activity.findViewById(R.id.routeNameInput);
+
+
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity)
                 .setCancelable(false)
                 .setPositiveButton(R.string.sendInvite, null)
@@ -44,6 +56,8 @@ public class ProposeWalkDialog {
 
         Button cancelButton = alert.getButton(AlertDialog.BUTTON_NEGATIVE);
         cancelButton.setOnClickListener(v -> alert.dismiss());
+
+
         return alert;
     }
 }
